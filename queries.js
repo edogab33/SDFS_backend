8309e4fc20a99d6c6c6c48adb38ea59651f734f7
edogab33 <edo.gab33@gmail.com>
2022-04-20 11:03:31 +0200

    Start to develop startSimulation

--- queries.js
+++ queries.js
@@ -1,3 +1,7 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { json } = require('body-parser')
+const { response } = require('express')
 const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'postgres',
@@ -6,6 +10,14 @@ const pool = new Pool({
   password: '',
   port: 5432,
 })
+const app = express()
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+app.use(bodyParser.json())
 
 function toGeoJson(rows) {
   var obj = {
@@ -19,7 +31,7 @@ function toGeoJson(rows) {
   for (i = 0; i < rows.length; i++) {
     var feature = {
       "type": "Feature",
-      "properties": {"region": i},
+      "properties": {"id": i, "fire": 0},
       "geometry":{
         "type": "Polygon",
         "coordinates": []
@@ -41,40 +53,91 @@ const getGrid = async (req, res, next) => {
   var coords = [parseInt(req.params.x0), parseInt(req.params.xn), parseInt(req.params.y0), parseInt(req.params.yn)]
   pool.query('SELECT swx, swy FROM satellitemaps WHERE (swx >= '+coords[0]+' AND swx <= '+coords[1]+') '+
     'AND (swy >= '+coords[2]+' AND swy <= '+coords[3]+')', (error, result) => {
-      if (error) {
-        res.status(500).send(error)
-      }
       data = result.rows
       grid = toGeoJson(data)
       res.header("Access-Control-Allow-Origin", "*");
       res.status(200).json(grid)
     })
+    .catch(e => {
+      res.status(500).send(e)
+    })
 }
 
 const startSimulation = async (req, res) => {
-  initialState = req.body.initialState
-  console.log(initialState)
-  res.status(200)
+  var jsonInitState = req.body
+  console.log(jsonInitState)
+  var simulationId = Math.floor(Math.random() * 100000000)
+  var swx = jsonInitState.features[Math.ceil(jsonInitState.features.length / 2)].geometry.coordinates[0][0][0]
+  var swy = jsonInitState.features[Math.ceil(jsonInitState.features.length / 2)].geometry.coordinates[0][0][1]
+  var initialtime = Date.now()
+
+  // TODO: parametrize xsize and ysize
+  var simulations_values = '('+simulationId+','+swx+','+swy+','+initialtime+',Test'+','+5+','+5+','+10+','+0.1+','+200+','+10+')'
+  simulations_sql = 'INSERT INTO simulations (simulationid, swx, swy, initialtime, placename, xsize, ysize, cellsize, timestep, horizon, snapshottime) '+
+    +simulations_values+';'
+
+  pool.query(simulations_sql).then((error, result) => {
+    console.log(result)
+  })
+  .catch(e => {
+    res.status(500).send(e)
+  })
+
+  var initialstate_values = '('
+  for (let i = 0; i < jsonInitState.features.length; i++) {
+    if (i != 0) {
+      values += ', ('
+    }
+    initialstate_values += simulationId+','
+    initialstate_values += jsonInitState.features[i].geometry.coordinates[0][0][0]+','
+    initialstate_values += jsonInitState.features[i].geometry.coordinates[0][0][1]+','
+    initialstate_values += jsonInitState.features[i].properties.fire+')'
+  }
+  initialstate_sql= 'INSERT INTO initialstate (simulationid, swx, swy, fire) VALUES '+initialstate_values+';'
+
+  pool.query(initialstate_sql).then((error, result) => {
+    console.log(result)
+  })
+  .catch(e => {
+    res.status(500).send(e)
+  })
+
+  pool.query(putRequest(simulationId, "start")).then((error, result) => {
+    console.log(result)
+    res.status(200).data(simulationId)
+  })
+  .catch(e => {
+    res.status(500).send(e)
+  })
 }
 
 const getCoords = (request, response) => {
   pool.query('SELECT swx, swy FROM satellitemaps', (error, result) => {
-    if (error) {
-      response.status(500).send(error)
-    }
     console.log(result)
     response.status(200).json(result.rows)
+  })
+  .catch(e => {
+    res.status(500).send(e)
   })
 }
 
 const getRequests = (request, response) => {
   pool.query('SELECT * FROM requests', (error, result) => {
-    if (error) {
-      res.status(500).send(error)
-    }
     response.status(200).json(result.rows)
   })
+  .catch(e => {
+    res.status(500).send(e)
+  })
 }
+
+function putRequest(simulationId, simcmd) {
+  // simcmd should be either "start" or "stop"
+  // returns a Pool.query object
+  sql_query = 'INSER INTO requests (simulationid, simcmd) VALUES '+simulationId+','+simcmd+';'
+  return sql_query
+}
+
+function getSimulatorSnapshot(simulationId) {}
 
 module.exports = {
   getRequests,
